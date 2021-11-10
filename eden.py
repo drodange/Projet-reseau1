dcbe6e758eb8553e911b261d06fe1d1deb5820e4
drodange <71694755+drodange@users.noreply.github.com>
2021-11-10 21:01:30 +0100

    Update eden.py

--- eden.py
+++ eden.py
@@ -71,6 +71,7 @@ def main():
     if mode == 'Serveur':
         Serveur2.debut()
         y = str(Serveur2.choixmap())
+        global y
         if y == "map1" : 
             map = map1
         if y == "map2" :
