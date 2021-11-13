b961982b131f33df811bc90e8fc6bf8378abcc59
drodange <71694755+drodange@users.noreply.github.com>
2021-11-13 19:23:19 +0100

    Update eden.py

--- eden.py
+++ eden.py
@@ -85,7 +85,7 @@ def main():
         msg = Serveur2.sc.recv(1024)
         x = msg.decode()
         print(x)
-        t = input("Quel personnage :" + 'x' )
+        t = input("Quel personnage : %s" %x )
         print(t)
     if mode == 'Client':
         Client2.debut()
