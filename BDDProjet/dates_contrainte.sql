0a9e7973aee44665ea5243dd84f897697fbbb352
drodange <71694755+drodange@users.noreply.github.com>
2021-11-15 22:48:21 +0100

    Delete BDDProjet directory

--- BDDProjet/dates_contrainte.sql
+++
@@ -1,3 +0,0 @@
--- puis appeler la fonction dans une contrainte d'integrite
-ALTER TABLE Dates.Edt ADD CONSTRAINT DatesPossibles
-  CHECK(Dates.DatesDebutFinEdt(idEns, idAmphi, date_debut, date_fin));
